aa92f857294443eefa59fe90440fa1c6e3e45522
yashselokar2025 <yash20selokar22@gmail.com>
2026-01-04 23:10:35 +0530

    Add files via upload

---
+++ Untitled5.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+struct student{
+	char name[20];
+	int roll;
+	float marks;
+};
+void update(struct student*s){
+	s->marks += 5;
+}
+void display(struct student s){
+	printf("Roll:%d\nName:%s\nMarks:%2f\n",s.roll,&s.name,&s.marks);
+}
+int main()
+{
+	struct student s1={"Animesh",84,78.45};
+	update(&s1);
+	display(s1);
+	
+}
